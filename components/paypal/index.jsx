ed366a30083da2fce4d599fb284c4bc3ae7f1dd0
CCS <abhishekyadav12@epicwebtechno.co.in>
2024-09-19 16:16:26 +0530

    payment option change revert

--- components/paypal/index.jsx
+++ components/paypal/index.jsx
@@ -43,6 +43,42 @@ const IndexPage = () => {
             <div className="col-lg-6 mt-5">
               <h2>If you already have a PayPal account:</h2>
               <p className="my-2">Click the button below to pay with PayPal.</p>
+
+              {/* <Script
+                src="https://www.paypal.com/sdk/js?client-id=AdsXcCsob5S29OyshWfQviL8Fz7bmDqG7cFxk2G88nt_-NR31IBcdxXmXOtTPfJfgbeL5aUqrdCRRsL0&vault=true&intent=subscription"
+                strategy="lazyOnload"
+                onLoad={() => {
+                  if (window.paypal) {
+                    window.paypal
+                      .Buttons({
+                        style: {
+                          shape: "rect",
+                          color: "gold",
+                          layout: "vertical",
+                          label: "subscribe",
+                        },
+                        createSubscription: function (data, actions) {
+                          return actions.subscription.create({
+                            plan_id:
+                              localStorage.getItem("student") === "true"
+                                ? "P-7UG8450201529235HM3M24OY" // student plan id
+                                : "P-92X34000FH3458811M3V4NQI", // employer plan id
+                          });
+                        },
+                        onApprove: function (data, actions) {
+                          alert(
+                            "You have successfully subscribed to " +
+                              data.subscriptionID
+                          );
+                        },
+                      })
+                      .render("#paypal-button-container");
+                  }
+                }}
+              /> */}
+
+              <div id="paypal-button-container"></div>
+
               <div id="paypal-button-container" className="mt-4"></div>
 
               <Script
@@ -71,42 +107,11 @@ const IndexPage = () => {
                   }
                 }}
               />
+              <Script />
             </div>
           </div>
         </div>
       </section>
-      {/* <section className="payment-section">
-        <div className="container">
-        
-          <Script
-            src="https://www.paypal.com/sdk/js?client-id=AdsXcCsob5S29OyshWfQviL8Fz7bmDqG7cFxk2G88nt_-NR31IBcdxXmXOtTPfJfgbeL5aUqrdCRRsL0&vault=true&intent=subscription"
-            strategy="lazyOnload"
-            onLoad={() => {
-              if (window.paypal) {
-                window.paypal
-                  .Buttons({
-                    createSubscription: function (data, actions) {
-                      return actions.subscription.create({
-                        plan_id:
-                          localStorage.getItem("student") === "true"
-                            ? "P-7UG8450201529235HM3M24OY" // student plan id
-                            : "P-1AJ82536CF083471TM3MYKIQ", // employer plan id
-                      });
-                    },
-                    onApprove: function (data, actions) {
-                      alert(
-                        "You have successfully subscribed to " +
-                          data.subscriptionID
-                      );
-                    },
-                  })
-                  .render("#paypal-button-container");
-              }
-            }}
-          />
-          <div id="paypal-button-container"></div>
-        </div>
-      </section> */}
     </>
   );
 };
